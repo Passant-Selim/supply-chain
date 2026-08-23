33dbf6c7113559c38d3fb565ed7d8138a94f5574
Mariam Gameel <mariamgameel51@gmail.com>
2026-08-23 08:54:06 +0300

    auth

---
+++ backend/src/middlewares/globalErrorHandler.js
@@ -0,0 +1,79 @@
+const AppError = require("../utils/appError");
+
+
+const handleCastErrorDB = (error) => {
+    const message = `Invalid ${error.path}: ${error.value}`;
+    return new AppError(message, 400);
+};
+
+
+const handleDuplicateFieldsDB = (error) => {
+    const field = Object.keys(error.keyValue)[0];
+    const message = `${field} already exists, please use another value`;
+    return new AppError(message, 400);
+};
+
+
+const handleValidationErrorDB = (error) => {
+    const errors = Object.values(error.errors).map((el) => el.message);
+    const message = `Invalid input data: ${errors.join(". ")}`;
+    return new AppError(message, 400);
+};
+
+
+const handleJWTError = () =>
+    new AppError("Invalid token, please log in again", 401);
+
+
+const handleJWTExpiredError = () =>
+    new AppError("Your session has expired, please log in again", 401);
+
+
+const sendErrorDev = (error, res) => {
+    res.status(error.statusCode).json({
+        success: false,
+        status: error.status,
+        message: error.message,
+        error,
+        stack: error.stack,
+    });
+};
+
+
+const sendErrorProd = (error, res) => {
+    if (error.isOperational) {
+        return res.status(error.statusCode).json({
+            success: false,
+            status: error.status,
+            message: error.message,
+        });
+    }
+    console.error("UNEXPECTED ERROR: ", error);
+    return res.status(500).json({
+        success: false,
+        status: "error",
+        message: "Something went wrong",
+    });
+};
+
+
+const globalErrorHandler = (error, req, res, next) => {
+    error.statusCode = error.statusCode || 500;
+    error.status = error.status || "error";
+
+    if (process.env.NODE_ENV === "development") {
+        sendErrorDev(error, res);
+    } else {
+        let err = { ...error, message: error.message };
+
+        if (err.name === "CastError") err = handleCastErrorDB(err);
+        if (err.code === 11000) err = handleDuplicateFieldsDB(err);
+        if (err.name === "ValidationError") err = handleValidationErrorDB(err);
+        if (err.name === "JsonWenTokenError") err = handleJWTError();
+        if (err.name === "TokenExpiredError") err = handleJWTExpiredError();
+
+        sendErrorProd(err, res);
+    }
+};
+
+module.exports = globalErrorHandler;
---
+++ backend/src/middlewares/protect.js
@@ -0,0 +1,44 @@
+const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
+const { verifyToken } = require("../utils/jwt");
+const User = require("../modules/user/userModel");
+
+
+const protect = catchAsync(async (req, res, next) => {
+    let token;
+
+    if (req.cookies && req.cookies.token) {
+        token = req.cookies.token;
+    } else if (req.headers.authorization?.startsWith("Bearer")) {
+        token = req.headers.authorization.split(" ")[1];
+    }
+
+    if (!token) {
+        return next(
+            new AppError("You are not logged in, please log in to get access", 401),
+        );
+    }
+
+    const decoded = verifyToken(token);
+
+    const currentUser = await User.findById(decoded.id).populate({
+        path: "role",
+        populate: { path: "permessions" },
+    });
+
+    if (!currentUser) {
+        return next(
+            new AppError("The user belonging to this token no longer exists", 401),
+        );
+    }
+
+    if (currentUser.status !== "active") {
+        return next(new AppError("This account is inactive", 401));
+    }
+
+    req.user = currentUser;
+    next();
+});
+
+
+module.exports = protect;
